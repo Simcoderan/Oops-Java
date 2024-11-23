5b54e8ad42d3330b2819fad2589dfb2f6306e63e
simran <simransingh956429@gmail.com>
2024-11-23 15:33:08 +0530

    Anonymous Object 1

--- Anonymousobj.java
+++ Anonymousobj.java
@@ -1,3 +1,28 @@
 public class Anonymousobj {
+
+    public static void main(String[] args) {
+        //only way to call any method is to create an obj
+        //Abc obj=new Abc();
+        //obj.show();
+
+        //we can write in same line as well
+        new Abc().show();   //can be used only onces
+
+        
+        
+    }
     
+}
+class Abc {
+
+    public Abc(){
+        System.out.println("In constructor Abc");
+    }
+
+    //method
+    public  void show(){            //since its not staic we need an instance obj  to call
+        System.out.println("hello");
+    }
+
+
 }
