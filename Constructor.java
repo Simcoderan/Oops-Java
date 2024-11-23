f530a36f4fd54afc1991685ef3d2f88e808dd50e
simran <simransingh956429@gmail.com>
2024-11-23 15:25:02 +0530

    constructor intro

---
+++ Constructor.java
@@ -0,0 +1,21 @@
+public class Constructor {
+    public static void main(String[] args) {
+        
+        Abc obj=new Abc(5);
+    }
+    
+}
+class Abc
+{
+    //how much memory needed is defined by constructor-jvm gives default constructor
+    //signature of that constructor is- 
+    public Abc(int i )    //special kind of method
+    {                    //it never returns anything so need of void 
+        System.out.println("In Constructor Abc");
+
+
+
+    }
+
+
+}
